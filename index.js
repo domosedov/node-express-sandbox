c42cf52f3f980d36040eb9a211667b396983c4b1
Aleksandr Grigorii <domosedov.dev@gmail.com>
2020-08-29 10:15:25 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
-const http = require("http");
+const path = require('path');
+const express = require('express');
+const expressHandlebars = require('express-handlebars');
+
+const app = express();
+
 const PORT = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello World");
+app.engine('.hbs', expressHandlebars({
+    defaultLayout: 'main.hbs',
+    extname: '.hbs'
+}))
+
+app.set('view engine', '.hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => res.render('home'));
+app.get('/about', (req, res) => res.render('about', {text: 'Hello Express'}));
+
+
+app.use((req, res) => {
+    res.status(404);
+    res.render('404');
 });
 
-server.listen(PORT, () => console.log(`Server run on port ${PORT}`));
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500);
+    res.render('500');
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server run on htpp://localhost:${PORT}`);
+});
